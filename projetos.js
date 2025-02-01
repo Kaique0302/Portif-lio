e5ae9cd7c1c38a30a5e35f1118bbc3d0c9d1a751
Kaique0302 <kaiquegamer0302@gmail.com>
2025-02-01 03:04:09 -0300

    Alteração de animações,rgbs,e cores de textos

---
+++ projetos.js
@@ -0,0 +1,13 @@
+// Efeito dinâmico nos projetos
+document.querySelectorAll('.projeto-card').forEach((card) => {
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        card.style.transform = `rotateY(${(x - rect.width / 2) / 20}deg) rotateX(${(rect.height / 2 - y) / 20}deg)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+    });
+});
